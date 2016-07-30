15f16fe35ab3aa21e55dc7a96ef0e7bfef7865c3
Gurpreet <gurpreetsk@hotmail.com>
2016-07-30 12:37:22 +0530

    - location fetching

--- app/src/main/java/gurpreetsk/me/magicbricks/Splash.java
+++ app/src/main/java/gurpreetsk/me/magicbricks/Splash.java
@@ -1,13 +1,104 @@
 package gurpreetsk.me.magicbricks;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class Splash extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.HashMap;
+
+public class Splash extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+    private static final String TAG = "SPLASH.java";
+    private HashMap<String, String> learning = new HashMap<>();
+    private GoogleApiClient mGoogleApiClient;
+    Location mLastLocation;
+    Double latitude, longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+//        learningDataDefine();
+
+        displayLocation();
+
+        startNextActivity();
+
+    }
+
+    private void startNextActivity() {
+
+        Intent intent = new Intent(Splash.this, MainActivity.class);
+        //intent.putExtra();
+        startActivity(intent);
+
+    }
+
+    private void displayLocation() {
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Toast.makeText(Splash.this, "Please provide location permission.", Toast.LENGTH_SHORT).show();
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+            latitude = mLastLocation.getLatitude();
+            longitude = mLastLocation.getLongitude();
+            Log.v(TAG, mLastLocation.toString());
+            Toast.makeText(this, latitude + ", " + longitude, Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(Splash.this, "Couldn't get user location", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(Splash.this, "Connection suspended", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(Splash.this, "Connection Failed", Toast.LENGTH_SHORT).show();
     }
 }
