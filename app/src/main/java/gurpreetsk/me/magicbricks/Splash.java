e50aad004a4ea9498e8f5205c49eda526a7f264a
Gurpreet <gurpreetsk@hotmail.com>
2016-07-30 13:25:35 +0530

    - chatbot learning initiated

--- app/src/main/java/gurpreetsk/me/magicbricks/Splash.java
+++ app/src/main/java/gurpreetsk/me/magicbricks/Splash.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -87,7 +88,7 @@ public class Splash extends AppCompatActivity implements GoogleApiClient.Connect
             longitude = mLastLocation.getLongitude();
             Log.v(TAG, mLastLocation.toString());
             Toast.makeText(this, latitude + ", " + longitude, Toast.LENGTH_SHORT).show();
-        }else{
+        } else {
             Toast.makeText(Splash.this, "Couldn't get user location", Toast.LENGTH_SHORT).show();
         }
     }
@@ -103,21 +104,116 @@ public class Splash extends AppCompatActivity implements GoogleApiClient.Connect
     }
 
 
-
     private void learningDataDefine() {
 
-        //starting call to bot: level-0
-        learning.put("hi", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
-        learning.put("hello", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
-        learning.put("/hello", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
-        learning.put("start", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
-        learning.put("/start", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
-        learning.put("bonjour", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                //For custom query
+                learning.put("suggest", "Please enter your query\nTips: Keep it simple and include things like location, budget, rooms, etc.");
+                learning.put("find", "Please enter your query\nTips: Keep it simple and include things like location, budget, rooms, etc.");
+                learning.put("locate", "Please enter your query\nTips: Keep it simple and include things like location, budget, rooms, etc.");
+                learning.put("magic", "Please enter your query and wait for it \nTips: Keep it simple and include things like location, budget, rooms, etc.");
+
+
+                //starting call to bot: level-0
+                learning.put("hi", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
+                learning.put("hello", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
+                learning.put("/hello", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
+                learning.put("start", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
+                learning.put("/start", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
+                learning.put("bonjour", "Hello!  Welcome to PropertyBot. Please enter your query or select one:\n1. Location\n2. Budget\n3. No. of rooms");
+
+                //level-1 calls
+                learning.put("1", "location");
+                learning.put("1.", "location");
+                learning.put("location", "location");
+
+                learning.put("2", "Choose a budget option:\na. under 5000\nb. 5000-10000\nc. 10000-15000\nd. 15000-20000");
+                learning.put("2.", "Choose a budget option:\na. under 5000\nb. 5000-10000\nc. 10000-15000\nd. 15000-20000");
+                learning.put("budget", "Choose a budget option:\na. under 5000\nb. 5000-10000\nc. 10000-15000\nd. 15000-20000");
+
+                learning.put("3", "1BHK\n2BHK\n3BHK");
+                learning.put("3.", "1BHK\n2BHK\n3BHK");
+                learning.put("rooms", "1BHK\n2BHK\n3BHK");
+                learning.put("no of rooms", "1BHK\n2BHK\n3BHK");
+                learning.put("no. of rooms", "1BHK\n2BHK\n3BHK");
+                learning.put("number of rooms", "1BHK\n2BHK\n3BHK");
+
+                //level-2 calls
+                learning.put("a", "Getting locations near you...");
+                learning.put("b", "Getting locations near you...");
+                learning.put("c", "Getting locations near you...");
+                learning.put("5000", "Getting locations near you...");
+                learning.put("<5000", "Getting locations near you...");
+                learning.put("under 5000", "Getting locations near you...");
+                learning.put("less than 5000", "Getting locations near you...");
+                learning.put("5000-10000", "Getting locations near you...");
+                learning.put("10000-15000", "Getting locations near you...");
+                learning.put("15000-20000", "Getting locations near you...");
+                learning.put("b/w 5000-10000", "Getting locations near you...");
+                learning.put("b/w 10000-15000", "Getting locations near you...");
+                learning.put("b/w 10000-20000", "Getting locations near you...");       //for budget details
+
+                learning.put("1bhk", "Getting locations near you...");
+                learning.put("2bhk", "Getting locations near you...");
+                learning.put("3bhk", "Getting locations near you...");
+                learning.put("1 bhk", "Getting locations near you...");
+                learning.put("2 bhk", "Getting locations near you...");
+                learning.put("3 bhk", "Getting locations near you...");          //these 3 for space details
+
+                //level-3 calls
 
 
 
+                //whether available on rent or not
+                learning.put("rent", "rent");
+                learning.put("buy", "buy");
+
+
+            }
+        });
+
+        startNextActivity();
 
     }
+}
 
 
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+learning.put("a", "Getting locations near you...");
+                learning.put("b", "Getting locations near you...");
+                learning.put("c", "Getting locations near you...");
+                learning.put("5000", "Getting locations near you...");
+                learning.put("<5000", "Getting locations near you...");
+                learning.put("less than 5000", "Getting locations near you...");
+                learning.put("5000-10000", "Getting locations near you...");
+                learning.put("b/w 5000-10000", "Getting locations near you...");
+                learning.put("10000-20000", "Getting locations near you...");
+                learning.put("b/w 10000-20000", "Getting locations near you...");
+                learning.put(">20000", "Getting matching locations near you...");
+                learning.put("above 20000", "Getting matching locations near you...");
+                learning.put("20000", "Getting matching locations near you...");       //these for budget options
+
+                learning.put("1bhk", "Getting locations near you...");
+                learning.put("2bhk", "Getting locations near you...");
+                learning.put("3bhk", "Getting locations near you...");
+                learning.put("1 bhk", "Getting locations near you...");
+                learning.put("2 bhk", "Getting locations near you...");
+                learning.put("3 bhk", "Getting locations near you...");          //these 3 for space details
+
+ */
